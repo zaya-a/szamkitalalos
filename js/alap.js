32a8de53ba9d93abd21cd9d31785504d685335d4
DrTlKl <cs.zsombi03@gmail.com>
2021-05-03 14:05:17 +0200

    bootstrap hozzáadása; html szerkezet felépítéseŁ; js kód megírása

--- js/alap.js
+++ js/alap.js
@@ -1,9 +1,35 @@
 window.addEventListener("load",myInit,false);
 
 function $ (id){
-    document.getElementById(id);
+    return document.getElementById(id);
 }
 
 function myInit (){
-    
+    let ellenorzo = $("tippbtn");
+    ellenorzo.addEventListener("click", ellenorzes, false);
+    let ujjatek = $("ujjatek");
+    ujjatek.addEventListener("click", reload, false);
+}
+
+let probaDb=0;
+let random = Math.floor((Math.random()*1024)+1);
+console.log(random);
+
+function ellenorzes(){
+    probaDb++;
+    if($("tipp").value == random){
+        $("eredmeny").innerText="=";
+        $("stat").innerText="ennyiszer próbálkozott: "+probaDb;
+        $("ujjatek").style.visibility="visible";
+    }
+    if($("tipp").value > random){
+        $("eredmeny").innerText="lefelényíl";
+    }
+    if($("tipp").value < random){
+        $("eredmeny").innerText="felfelényíl";
+    }
+}
+
+function reload(){
+    window.location.reload();
 }
